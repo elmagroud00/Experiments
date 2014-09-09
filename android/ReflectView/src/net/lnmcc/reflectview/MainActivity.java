b74a9bd959c7e58b49b41ee2ee1b1790e5f7ee77
lnmcc <lnmcc@hotmail.com>
2014-09-09 11:34:11 +0800

    testBattery TestWindowManager TestMultiLayout TestIntent TestExceptRecentReflectView

--- android/ReflectView/src/net/lnmcc/reflectview/MainActivity.java
+++ android/ReflectView/src/net/lnmcc/reflectview/MainActivity.java
@@ -1,15 +1,63 @@
 package net.lnmcc.reflectview;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+    ReflectView reflectView; 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        reflectView = (ReflectView)findViewById(R.id.reflectView);
+        
+        InputStream is = null;
+        Bitmap bmp = null;
+        
+        try {
+            is = getAssets().open("1.jpg");
+            bmp = BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        if(bmp != null) {
+            reflectView.setOrigPicure(bmp);
+        }
+        
+        try {
+            is = getAssets().open("1.jpg");
+            bmp = BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        if(bmp != null) {
+            reflectView.setReflectPicture(bmp);
+        }
+
+    }
+    
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+    }
+    
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
     }
 }
--- android/TestMultiLayout/src/net/lnmcc/testmultilayout/MainActivity.java
+++ android/TestMultiLayout/src/net/lnmcc/testmultilayout/MainActivity.java
@@ -5,160 +5,174 @@ import java.io.IOException;
 import android.app.Activity;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.EventLogTags.Description;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
-	private static final int LARGE_MOVE = 60;
+    private static final int LARGE_MOVE = 60;
 
-	FrameLayout firstContatiner;
-	FrameLayout secondContainer;
-	GestureDetector gestureDetector;
+    FrameLayout firstContatiner;
+    GestureDetector gestureDetector;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		firstContatiner = (FrameLayout) findViewById(R.id.first_LL);
-		secondContainer = (FrameLayout) findViewById(R.id.second_LL);
+        firstContatiner = (FrameLayout) findViewById(R.id.first_LL);
 
-		gestureDetector = new GestureDetector(this,
-				new GestureDetector.OnGestureListener() {
+        gestureDetector = new GestureDetector(this,
+                new GestureDetector.OnGestureListener() {
 
-					@Override
-					public boolean onSingleTapUp(MotionEvent e) {
-						// TODO Auto-generated method stub
-						return false;
-					}
+                    @Override
+                    public boolean onSingleTapUp(MotionEvent e) {
+                        // TODO Auto-generated method stub
+                        return false;
+                    }
 
-					@Override
-					public void onShowPress(MotionEvent e) {
-						// TODO Auto-generated method stub
+                    @Override
+                    public void onShowPress(MotionEvent e) {
+                        // TODO Auto-generated method stub
 
-					}
+                    }
 
-					@Override
-					public boolean onScroll(MotionEvent e1, MotionEvent e2,
-							float distanceX, float distanceY) {
-						// TODO Auto-generated method stub
-						return false;
-					}
+                    @Override
+                    public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                            float distanceX, float distanceY) {
+                        // TODO Auto-generated method stub
+                        return false;
+                    }
 
-					@Override
-					public void onLongPress(MotionEvent e) {
-						// TODO Auto-generated method stub
+                    @Override
+                    public void onLongPress(MotionEvent e) {
+                        // TODO Auto-generated method stub
 
-					}
+                    }
 
-					@Override
-					public boolean onFling(MotionEvent e1, MotionEvent e2,
-							float velocityX, float velocityY) {
-						if (e1.getY() - e2.getY() > LARGE_MOVE) {
-							return true;
-						} else if (e2.getY() - e1.getY() > LARGE_MOVE) {
-							return true;
-						} else if (e1.getX() - e2.getX() > LARGE_MOVE) {
-							return true;
-						} else if (e2.getX() - e1.getX() > LARGE_MOVE) {
-							return true;
-						}
+                    @Override
+                    public boolean onFling(MotionEvent e1, MotionEvent e2,
+                            float velocityX, float velocityY) {
+                        if (e1.getY() - e2.getY() > LARGE_MOVE) {
+                            return true;
+                        } else if (e2.getY() - e1.getY() > LARGE_MOVE) {
+                            return true;
+                        } else if (e1.getX() - e2.getX() > LARGE_MOVE) {
+                            return true;
+                        } else if (e2.getX() - e1.getX() > LARGE_MOVE) {
+                            return true;
+                        }
 
-						return false;
-					}
+                        return false;
+                    }
 
-					@Override
-					public boolean onDown(MotionEvent e) {
-						// TODO Auto-generated method stub
-						return false;
-					}
-				});
-	}
+                    @Override
+                    public boolean onDown(MotionEvent e) {
+                        // TODO Auto-generated method stub
+                        return false;
+                    }
+                });
+    }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		return false;
-	}
-	
-	@Override
-	protected void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-	}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
 
-	@Override
-	protected void onResume() {
-		getView();
-		super.onResume();
-	}
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+    }
 
-	void getView() {
-		View view = LayoutInflater.from(this).inflate(R.layout.item, null);
-		ImageView image = (ImageView) view.findViewById(R.id.image);
-		try {
-			image.setImageBitmap(BitmapFactory.decodeStream(getAssets().open(
-					"1.jpg")));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		firstContatiner.addView(view);
+    @Override
+    protected void onResume() {
+        getView();
+        super.onResume();
+    }
 
-		View view2 = LayoutInflater.from(this).inflate(R.layout.item, null);
-		ImageView image2 = (ImageView) view2.findViewById(R.id.image);
-		try {
-			image2.setImageBitmap(BitmapFactory.decodeStream(getAssets().open(
-					"2.jpg")));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		secondContainer.addView(view2);
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        float y1, y2;
 
-		View view3 = LayoutInflater.from(this).inflate(R.layout.item, null);
-		ImageView image3 = (ImageView) view3.findViewById(R.id.image);
-		try {
-			image3.setImageBitmap(BitmapFactory.decodeStream(getAssets().open(
-					"3.jpg")));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		firstContatiner.addView(view3);
-		/*view3.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = event.getAction();
 
-			@Override
-			public void onClick(View v) {
-				firstContatiner.removeView(v);
-				firstContatiner.postInvalidate();
-			}
-		});
-		*/
-		view3.setOnTouchListener(new View.OnTouchListener() {
-			float y1, y2;
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				int action = event.getAction();
-				
-				switch(action) {
-				case MotionEvent.ACTION_DOWN:
-					y1 = event.getY();
-					break;
-				case MotionEvent.ACTION_MOVE:
-					y2 = event.getY();
-					if(y1 - y2 > 200) {
-						firstContatiner.removeView(v);
-						firstContatiner.postInvalidate();
-					}
-					break;
-				case MotionEvent.ACTION_CANCEL:
-					break;
-				}
-				return true;
-			}
-			
-		});
-	}
+            switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                y2 = event.getY();
+                if (y1 - y2 > 200) {
+                    firstContatiner.removeView(v);
+                    firstContatiner.postInvalidate();
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                break;
+            }
+
+            return true;
+        }
+    };
+
+    class ViewHolder {
+        int Id;
+    }
+    
+    void getView() {
+        ViewHolder holder;
+        View view = LayoutInflater.from(this).inflate(R.layout.item, null);
+
+        ImageView image = (ImageView) view.findViewById(R.id.image);
+        try {
+            image.setImageBitmap(BitmapFactory.decodeStream(getAssets().open(
+                    "1.jpg")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        holder = new ViewHolder();
+        holder.Id = 1;
+        view.setTag(holder);
+        view.setOnTouchListener(touchListener);
+        firstContatiner.addView(view);
+
+        View view2 = LayoutInflater.from(this).inflate(R.layout.item, null);
+        ImageView image2 = (ImageView) view2.findViewById(R.id.image);
+        try {
+            image2.setImageBitmap(BitmapFactory.decodeStream(getAssets().open(
+                    "2.jpg")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        holder = new ViewHolder();
+        holder.Id = 2;
+        view2.setTag(holder);
+        view2.setOnTouchListener(touchListener);
+        firstContatiner.addView(view2);
+
+        View view3 = LayoutInflater.from(this).inflate(R.layout.item, null);
+        ImageView image3 = (ImageView) view3.findViewById(R.id.image);
+        try {
+            image3.setImageBitmap(BitmapFactory.decodeStream(getAssets().open(
+                    "3.jpg")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        holder = new ViewHolder();
+        holder.Id = 3;
+        view3.setTag(holder);
+        view3.setOnTouchListener(touchListener);
+        firstContatiner.addView(view3);
+
+    }
 }
