e67a788c4c9679742babcf013f5087f834b600d7
lnmcc <lnmcc@hotmail.com>
2014-09-11 19:45:24 +0800

    ...

--- android/estGetContentImage/src/net/lnmcc/testgetcontentimage/MainActivity.java
+++ android/estGetContentImage/src/net/lnmcc/testgetcontentimage/MainActivity.java
@@ -1,11 +1,18 @@
 package net.lnmcc.testgetcontentimage;
 
+import java.io.File;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,7 +23,38 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 
+    private static final String TAG = "TestGetContentImage";
+    private static final int REQUEST_CODE = 1;
+
     Button openImageBtn;
+    Uri tempPhotoUri;
+
+    private Uri getTempUri() {
+        Uri tempPhotoUri = Uri.fromFile(getTempFile());
+        return tempPhotoUri;
+    }
+
+    private File getTempFile() {
+        if (isSDCARDMounted()) {
+            File f = new File(Environment.getExternalStorageDirectory(),
+                    "temp.jpg");
+            try {
+                f.createNewFile();
+            } catch (IOException e) {
+                Log.d(TAG, "Create temp file " + e.toString());
+            }
+            return f;
+        }
+        return null;
+    }
+
+    private boolean isSDCARDMounted() {
+        String status = Environment.getExternalStorageState();
+        if (status.equals(Environment.MEDIA_MOUNTED))
+            return true;
+        else
+            return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,81 +67,37 @@ public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     public void onClick(View v) {
-        final Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
-        intent.setType("image/*");
-        intent.putExtra("crop", true);
-        intent.putExtra("scale", true);
-        intent.putExtra("scaleUpIfNeeded", false);
-        intent.putExtra("layout_witdh", -1);
-        intent.putExtra("layout_height", -1);
-        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
 
         final Display display = getWindowManager().getDefaultDisplay();
         boolean isPortrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
-        
+
         Point screenDimension = new Point();
         display.getSize(screenDimension);
-        Toast.makeText(this, "Default Display:X = " + screenDimension.x +
-                " Y = " + screenDimension.y, Toast.LENGTH_SHORT).show();
+        Toast.makeText(
+                this,
+                "Default Display:X = " + screenDimension.x + " Y = "
+                        + screenDimension.y, Toast.LENGTH_SHORT).show();
+
+        final Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
+        intent.setType("image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("return-data", false);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, getTempUri());
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+
         int width = screenDimension.x;
         int height = screenDimension.y;
-        intent.putExtra("aspectX", width);
-        intent.putExtra("aspectY", height);
-        startActivity(intent);
+        intent.putExtra("outputX", width);
+        intent.putExtra("outputY", height);
+
+        startActivityForResult(intent, REQUEST_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
+        Log.d(TAG, "onActivityResult: resultCode: " + resultCode + " data: "
+                + data.toString());
+        Log.d(TAG, "data.getData: " + data.getData().toString());
         super.onActivityResult(requestCode, resultCode, data);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
