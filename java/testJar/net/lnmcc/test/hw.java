9aabcb0d666b7fe10867753db89996fa79329105
lnmcc <lnmcc@hotmail.com>
2014-09-19 17:52:05 +0800

    add TestJarAndProguard

---
+++ java/testJar/net/lnmcc/test/hw.java
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: hw.java
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 17 Sep 2014 11:49:16 AM CST
+ ************************************************************************/
+public class hw {
+    public static void main(String args) {
+
+    }
+}
