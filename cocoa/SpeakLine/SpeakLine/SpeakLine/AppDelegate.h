b2c386cdf97a8dbd2e937215bab0d4b7ecca02c8
lnmcc <lnmcc@hotmail.com>
2014-10-24 22:41:57 +0800

    SpeakLine

--- cocoa/SpeakLine/SpeakLine/SpeakLine/AppDelegate.h
+++ cocoa/SpeakLine/SpeakLine/SpeakLine/AppDelegate.h
@@ -8,14 +8,18 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSSpeechSynthesizerDelegate, NSTableViewDataSource, NSTableViewDelegate>
 {
+    NSArray *_voices;
     NSSpeechSynthesizer *_speechSynth;
 }
 
 - (IBAction)stopIt:(id)sender;
 - (IBAction)sayIt:(id)sender;
 @property (weak) IBOutlet NSTextField *textField;
+@property (weak) IBOutlet NSButton *stopButton;
+@property (weak) IBOutlet NSButton *speakButton;
+@property (weak) IBOutlet NSTableView *tableView;
 
 @end
 
--- cocoa/SpeakLine/SpeakLine/SpeakLine/AppDelegate.m
+++ cocoa/SpeakLine/SpeakLine/SpeakLine/AppDelegate.m
@@ -18,14 +18,52 @@
 
 @synthesize window = _window;
 @synthesize textField = _textField;
+@synthesize tableView = _tableView;
 
 -(id)init {
     self = [super init];
     if(self) {
         NSLog(@"init");
         _speechSynth = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
+        [_speechSynth setDelegate:self];
+        _voices = [NSSpeechSynthesizer availableVoices];
     }
     return self;
+}
+
+-(void)awakeFromNib {
+    NSString *defaultVoice = [NSSpeechSynthesizer defaultVoice];
+    NSInteger defaultRow = [_voices indexOfObject:defaultVoice];
+    NSIndexSet *indices = [NSIndexSet indexSetWithIndex:defaultRow];
+    [_tableView selectRowIndexes:indices byExtendingSelection:NO];
+    [_tableView scrollRowToVisible:defaultRow];
+}
+
+-(void)tableViewSelectionDidChange:(NSNotification *)notification {
+    NSInteger row = [_tableView selectedRow];
+    if(row == -1)
+        return;
+    
+    NSString *selectedVoice = [_voices objectAtIndex:row];
+    [_speechSynth setVoice:selectedVoice];
+    NSLog(@"new voice = %@", selectedVoice);
+}
+
+-(NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    return (NSInteger)[_voices count];
+}
+
+-(id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    NSString *v = [_voices objectAtIndex:row];
+    NSDictionary *dict = [NSSpeechSynthesizer attributesForVoice:v];
+    return [dict objectForKey:NSVoiceName];
+}
+
+-(void)speechSynthesizer:(NSSpeechSynthesizer *)sender didFinishSpeaking:(BOOL)finishedSpeaking {
+    NSLog(@"finished Speaking = %d", finishedSpeaking);
+    [_stopButton setEnabled:NO];
+    [_speakButton setEnabled:YES];
+    [_tableView setEnabled:YES];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
@@ -39,6 +77,7 @@
 - (IBAction)stopIt:(id)sender {
     NSLog(@"stopping");
     [_speechSynth stopSpeaking];
+    [_tableView setEnabled:YES];
 }
 
 - (IBAction)sayIt:(id)sender {
@@ -49,5 +88,9 @@
     }
     [_speechSynth startSpeakingString:string];
     NSLog(@"Have started to say: %@", string);
+    
+    [_stopButton setEnabled:YES];
+    [_speakButton setEnabled:NO];
+    [_tableView setEnabled:NO];
 }
 @end
