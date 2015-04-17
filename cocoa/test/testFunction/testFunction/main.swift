933967a9b6732d5f2eb20c25f9ef55df64bedc19
lnmcc <lnmcc@hotmail.com>
2015-04-18 00:40:56 +0800

    ...

---
+++ cocoa/test/testFunction/testFunction/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testFunction
+//
+//  Created by sijiewang on 4/15/15.
+//  Copyright (c) 2015 lnmcc. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
