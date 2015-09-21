324a81c1119e2ba370f2b3139d27cc9b6f7b2586
lnmcc <lnmcc@hotmail.com>
2015-09-21 23:29:47 +0800

    ...

--- IOS/Homepwner/Homepwner/BNRImageStore.m
+++ IOS/Homepwner/Homepwner/BNRImageStore.m
@@ -41,8 +41,16 @@
     self = [super init];
     if (self) {
         _dictionary = [[NSMutableDictionary alloc]init];
+        
+        NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+        [nc addObserver:self selector:@selector(clearCache:) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
     }
     return self;
+}
+
+- (void)clearCache:(NSNotification*)note {
+    NSLog(@"flushing %d images out of the cache", [self.dictionary count]);
+    [self.dictionary removeAllObjects];
 }
 
 - (instancetype) init {
