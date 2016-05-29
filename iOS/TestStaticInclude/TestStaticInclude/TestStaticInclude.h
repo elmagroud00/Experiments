cd0cb93e8ae5bd6caafd64f83875989817c70815
lnmcc <lnmcc@hotmail.com>
2016-05-29 11:36:36 +0800

    TestStaticInclude

---
+++ iOS/TestStaticInclude/TestStaticInclude/TestStaticInclude.h
@@ -0,0 +1,19 @@
+//
+//  TestStaticInclude.h
+//  TestStaticInclude
+//
+//  Created by sijiewang on 5/29/16.
+//  Copyright © 2016 lnmcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestStaticInclude.
+FOUNDATION_EXPORT double TestStaticIncludeVersionNumber;
+
+//! Project version string for TestStaticInclude.
+FOUNDATION_EXPORT const unsigned char TestStaticIncludeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestStaticInclude/PublicHeader.h>
+
+
